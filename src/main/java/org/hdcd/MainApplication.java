f7a3770b81da890a450c5b4e93637c377e5dec35
jhbae528 <jhbae5280@gmail.com>
2022-02-12 09:20:21 +0900

    .

--- src/main/java/org/hdcd/MainApplication.java
+++ src/main/java/org/hdcd/MainApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainApplication {
 
 	public static void main(String[] args) {
-		//test2
+		//test23
 		SpringApplication.run(MainApplication.class, args);
 	}
 
